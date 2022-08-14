5663876b4589f194df15b46fdaab3a0de409d98a
Edwin0204 <edwins0204@gmail.com>
2022-08-14 10:12:53 -0500

    made the options inquirer

--- server.js
+++ server.js
@@ -4,3 +4,66 @@ const cTable = require('console.table');
 const db = require('./db/connection');
 
 
+
+const chooseRequest = () => {
+    console.log('Welcome to the Employee Tracker');
+    inquirer.prompt([
+        {
+           type: 'rawlist',
+           message: 'What would you like to do?',
+           name: 'options',
+           choices: ['View all departments', 
+                     'View all roles', 
+                     'View all employees', 
+                     'Add a department', 
+                     'Add a role', 
+                     'Add an employee', 
+                     'Update an employee role', 
+                     'Finish' ]
+        }
+     ])
+     .then((userChoice) => {
+        switch(userChoice.options){
+   
+         case 'View all departments':
+          sqlFunctions.viewDepts();
+           chooseRequest();
+         break;
+         
+         case 'View all roles':
+         sqlFunctions.viewRoles();
+         chooseRequest();
+         break;
+   
+         case 'View all employees':
+         sqlFunctions.viewEmployees();
+         chooseRequest();
+         break;
+   
+         case 'Add a department':
+           createDept();
+         break;
+   
+         case 'Add a role':
+           createRole();
+         break;
+   
+         case 'Add an employee':
+           createEmployee();
+         break;
+   
+         case 'Update an employee role':
+           updateEmployee();
+         break;
+         
+         case 'Finish':
+         finish();
+         break;
+        }
+     })
+   }
+
+
+
+   
+   chooseRequest();
