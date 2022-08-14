8ac4fb76735419233db4bde25e561482a5cd2caa
Edwin0204 <edwins0204@gmail.com>
2022-08-14 13:39:50 -0500

    Employees Query

--- db/query_lib.js
+++ db/query_lib.js
@@ -67,7 +67,7 @@ function addEmployeeUpdate (newIdRole, employeeId){
 
 
 //View all Roles
-async function viewRoles() {
+  async function viewRoles() {
   const [rows,fields] = await db.promise().query(
   `SELECT roles.id, roles.title AS Title, department.name AS Department,  roles.salary AS Salary
   FROM roles
@@ -78,6 +78,20 @@ async function viewRoles() {
   console.log('Press Space or move key Up or Down to select another choice');
 }
 
+//View all Employees
+  async function viewEmployees() {
+  const [rows,fields] = await db.promise().query(
+  `SELECT employee.id, employee.first_name AS FirstName, employee.last_name AS LastName, roles.title AS Title, 
+  department.name AS Department, roles.salary AS Salary, CONCAT(manager.first_name, " ", manager.last_name) AS Manager 
+  FROM employees employee
+  LEFT JOIN employees Manager ON employee.manager_id = manager.id
+  LEFT JOIN roles ON employee.role_id = roles.id 
+  LEFT JOIN department ON roles.department_id = department.id;`)
+  .catch(err=>{console.log('Error conecting to DB')});
+  console.table(rows); 
+  console.log('Press Space or move key Up or Down to select another choice');
+}
+
   module.exports = {
     addDept,
     addRole,
@@ -85,4 +99,5 @@ async function viewRoles() {
     addEmployeeUpdate, 
     viewDepts,
     viewRoles,
+    viewEmployees
     }
