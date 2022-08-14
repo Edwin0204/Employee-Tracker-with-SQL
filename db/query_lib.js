1306c6185479064cc0436a8b81b7017a8d78733b
Edwin0204 <edwins0204@gmail.com>
2022-08-14 11:30:29 -0500

    Query Role

--- db/query_lib.js
+++ db/query_lib.js
@@ -6,10 +6,10 @@ const cTable = require('console.table');
 
 
 
-//QUERY newDept
+//CreateDept
 function addDept(createDept) {
     db.query(`
-    INSERT INTO department name;`, createDept, (err, result)=>{
+    INSERT INTO department(name) VALUES (?);`, createDept, (err, result)=>{
       if(err){
         console.log("Error conecting to DB");
       }
@@ -17,7 +17,20 @@ function addDept(createDept) {
   }
 
 
+  //CreateRole
+  function addRole(newRole, salary, deptId){
+  db.query(`INSERT INTO roles(title, salary, department_id) VALUES (?, ?, ?)`, [newRole, salary, deptId] ,(err, result)=>{
+    if(err){
+      console.log("Ups! Something went wrong when trying to connect to database");
+    } else{
+      console.log(`${newRole} role successfully created`);
+    }
+  });
+
+}
+
 
   module.exports = {
     addDept,
+    addRole,
     }
